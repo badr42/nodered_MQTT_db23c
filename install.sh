f831d3442971a3c956d1ecfbaac04365c5e495b8
badr42 <badr42@gmail.com>
2023-06-01 01:15:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,28 +3,28 @@ export DB_PASSWORD=$1
 
 echo "***Installing Dependencies***" 
 
-sudo -s
-dnf install -y oraclelinux-developer-release-el8
+# sudo -s
+sudo dnf install -y oraclelinux-developer-release-el8
 
-dnf config-manager --set-enabled ol8_developer 
-dnf -y install oracle-database-preinstall-23c
+sudo dnf config-manager --set-enabled ol8_developer 
+sudo dnf -y install oracle-database-preinstall-23c
 
 echo "***Downloading DB RPM***" 
 
-wget https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
+sudo wget https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
 
 
 
 echo "***Installing DB 23c free***" 
 
-dnf -y localinstall oracle-database-free-23c-1.0-1.el8.x86_64.rpm
+sudo dnf -y localinstall oracle-database-free-23c-1.0-1.el8.x86_64.rpm
 
 
 #Configure the Database
 echo "***Configuring DB***" 
 
 export DB_PASSWORD=Tiger$tr0ng2023
-(echo "${DB_PASSWORD}"; echo "${DB_PASSWORD}";) | /etc/init.d/oracle-free-23c configure
+(echo "${DB_PASSWORD}"; echo "${DB_PASSWORD}";) | sudo /etc/init.d/oracle-free-23c configure
 
 echo "***DB INSTALLED***" 
 
