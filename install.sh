1f9915cc7ed4d504d23ae2daf8ae064e5c48d64f
badr42 <badr42@gmail.com>
2023-06-01 22:51:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,39 +5,50 @@ echo "***DB Password=*** $DB_PASSWORD"
 
 echo "***Installing Dependencies***" 
 
-# sudo -s
+sudo yum update -y
 sudo dnf install -y oraclelinux-developer-release-el8
-
 sudo dnf config-manager --set-enabled ol8_developer 
 sudo dnf -y install oracle-database-preinstall-23c
 
-echo "***Downloading DB RPM***" 
 
+
+echo "***Downloading DB RPM***" 
 sudo wget https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
 
 
 
-echo "***Installing DB 23c free***" 
 
+echo "***Installing DB 23c free***" 
 sudo dnf -y localinstall oracle-database-free-23c-1.0-1.el8.x86_64.rpm
 
 
 #Configure the Database
 echo "***Configuring DB***" 
-
 export DB_PASSWORD=Tiger$tr0ng2023
 (echo "${DB_PASSWORD}"; echo "${DB_PASSWORD}";) | sudo /etc/init.d/oracle-free-23c configure
+
+echo "**** set up environment ****"
+echo "export ORACLE_SID=FREE" >> ~/.bashrc
+echo "export ORAENV_ASK=NO" >> ~/.bashrc
+echo ". /opt/oracle/product/23c/dbhomeFree/bin/oraenv" >> ~/.bashrc
+
 
 echo "***DB INSTALLED***" 
 
 
+
+
+
 #Install nodered
 echo "**Installing NODERED**"
+sudo yum remove nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install node
 
-
-
-sudo dnf install -y npm
+# sudo dnf install -y npm
 sudo npm install -g --unsafe-perm node-red
+
 
 echo "**adding firewall rules**"
 
@@ -54,14 +65,46 @@ echo "***availaing tnsnames.ora***"
 chmod 666 /opt/oracle/product/23c/dbhomeFree/network/admin/tnsnames.ora
 
 
+echo "*** installing instant client**"
+
+npm install oracledb
+
+
+
 
 echo "*** create the service ***"
 
 sudo npm install -g pm2
 
-sleep 5 
-
-pm2 start /usr/local/bin/node-red -- -v
+sleep 15
 
 
-pm2 startup systemd
+
+sudo cp /opt/oracle/product/23c/dbhomeFree/lib/libclntsh.so /opt/oracle/product/23c/dbhomeFree/instantclient/
+
+
+
+
+
+
+# create user for nodered on db
+
+echo " **** create user for nodered **** " 
+
+sudo su - oracle 
+# **set the environment variables**
+export ORACLE_SID=FREE 
+export ORAENV_ASK=NO 
+. /opt/oracle/product/23c/dbhomeFree/bin/oraenv
+cd $ORACLE_HOME/bin
+
+wget https://raw.githubusercontent.com/badr42/nodered_MQTT_db23c/main/createuser.sql
+
+# lsnrctl status
+./sqlplus -S / as sysdba @createuser.sql
+
+
+#start nodered
+
+# pm2 start /usr/local/bin/node-red -- -v
+# pm2 startup systemd
